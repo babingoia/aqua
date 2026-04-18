932cd5a34b09ed0f14909c6e91c285ef8ed403ec
babingoia <137658431+babingoia@users.noreply.github.com>
2026-04-17 21:39:59 -0400

    Polimento da câmera

--- assets/outros/camera.gd
+++ assets/outros/camera.gd
@@ -1,17 +1,23 @@
 extends Camera2D
 
+var player: Character
+var concha: Character
+var player_na_tela: VisibleOnScreenNotifier2D
+var concha_na_tela: VisibleOnScreenNotifier2D
+
+@export var zoom_smothness: float = 5
 @export var move_speed: float = 0.5
-@export var zoom_speed: float = 0.05
+@export var zoom_speed: float = 0.15
 @export var min_zoom: float = 0.4
 @export var max_zoom: float = 5
+
+var zoom_target: Vector2
+var zoom_current: Vector2
 var vec_min_zoom: Vector2
 var vec_max_zoom: Vector2
 var vec_zoom_speed: Vector2 
-var limits: Rect2
-var player: Character
-var player_na_tela: VisibleOnScreenNotifier2D
-var concha: Character
-var concha_na_tela: VisibleOnScreenNotifier2D
+var map_limits: Rect2
+
 
 func _ready() -> void:
 	await owner.ready
@@ -21,29 +27,30 @@ func _ready() -> void:
 	concha = get_node("../Concha")
 	concha_na_tela = concha.get_node("OnScreen")
 	zoom = Vector2(0.4, 0.4)
-	limits = get_node("../Limites").get_rect()
+	map_limits = get_node("../Limites").get_rect()
 	
 	print(player_na_tela, concha_na_tela)
 	
 	limit_enabled = true
-	limit_top = limits.position.y
-	limit_left = limits.position.x
-	limit_bottom = limits.end.y
-	limit_right = limits.end.x
+	limit_top = map_limits.position.y
+	limit_left = map_limits.position.x
+	limit_bottom = map_limits.end.y
+	limit_right = map_limits.end.x
 	
 	vec_max_zoom = Vector2(max_zoom, max_zoom)
 	vec_min_zoom = Vector2(min_zoom, min_zoom)
 	vec_zoom_speed = Vector2(zoom_speed, zoom_speed)
-	
+	zoom_target = zoom
+
 
 func _input(event: InputEvent) -> void:
 	if player_na_tela.is_on_screen() and concha_na_tela.is_on_screen():
 		
 		if event.is_action_pressed(Controls.ZOOM_IN) and zoom < vec_max_zoom:
-			zoom = clamp(zoom + vec_zoom_speed, vec_min_zoom, vec_max_zoom)
-		
+			zoom_target = clamp(zoom + vec_zoom_speed, vec_min_zoom, vec_max_zoom)
+			
 		if event.is_action_pressed(Controls.ZOOM_OUT) and zoom > vec_min_zoom:
-			zoom = clamp(zoom - vec_zoom_speed, vec_min_zoom, vec_max_zoom)
+			zoom_target = clamp(zoom - vec_zoom_speed, vec_min_zoom, vec_max_zoom)
 
 
 func _process(delta: float) -> void:
@@ -58,3 +65,8 @@ func _process(delta: float) -> void:
 	
 	position = lerp(position, center, move_speed)
 	
+	if !player_na_tela.is_on_screen() or !concha_na_tela.is_on_screen():
+		zoom_target = clamp(zoom - vec_zoom_speed, vec_min_zoom, vec_max_zoom)
+
+	if zoom != zoom_target:
+		zoom = lerp(zoom, zoom_target, zoom_smothness * delta)
